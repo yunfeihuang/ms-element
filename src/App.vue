59b9d5a7c29e91e99a307452ced95769c1522922
yunfei <yunfei.huang@qq.com>
2021-02-01 05:39:27 +0800

    优化

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
 <template>
   <div class="frame-docs">
-    <x-header/>
+    <header>
+      <el-row type="flex" class="wrapper">
+        <el-col :span="24">
+          <div class="logo">M</div>
+          <h2>米沃讯组件文档</h2>
+        </el-col>
+        <a class="link" target="_blank" href="/demos.html">管理后台示例</a>
+      </el-row>
+    </header>
     <div class="wrapper">
       <el-row :gutter="20">
         <el-col :span="5">
-          <!--
-          <el-scrollbar wrap-class="sidebar-scrollbar-wrap">
-            <sidebar/>
-          </el-scrollbar>
-          -->
-          <sidebar/>
+          <aside>
+            <div v-for="(item,index) in navConfig" :key="index">
+              <template v-if="item.children">
+                <h4>{{item.title}}</h4>
+                <router-link v-for="(childItem,childIndex) in item.children" class="nav-link" :to="childItem.path" :key="childIndex">{{childItem.title}}</router-link>
+              </template>
+              <router-link style="font-weight:bold;" v-else class="nav-link" :to="item.path" >{{item.title}}</router-link>
+            </div>
+          </aside>
         </el-col>
         <el-col :span="19">
           <div class="markdown-body">
@@ -23,18 +34,15 @@
 </template>
 
 <script>
-import XHeader from './examples/XHeader'
-import XFooter from './examples/XFooter'
-import Sidebar from './examples/Sidebar'
 // import markdown from './markdown.md'
 // import 'highlight.js/styles/github.css'
 // import 'github-markdown-css'
-
+import navConfig from './nav.config'
 export default {
-  components: {
-    XHeader,
-    XFooter,
-    Sidebar
+  data () {
+    return {
+      navConfig
+    }
   },
   mounted () {
     window.onhashchange = () => {
@@ -47,6 +55,71 @@ export default {
   }
 }
 </script>
+<style lang="scss" scoped>
+  aside{
+    line-height:1;
+    h4{
+      margin-top:15px;
+      margin-bottom:2px;
+      font-size:12px;
+      color:$--color-text-secondary;
+      font-weight: normal;
+    }
+    a{
+      display:block;
+      color:#666;
+      padding:10px 0;
+      &:hover{
+        color:$--color-primary;
+      }
+      &.router-link-exact-active{
+        color:$--color-primary;
+        font-weight: bold;
+      }
+    }
+  }
+  header{
+    div.logo{
+      background: #409eff;
+      border-radius:10px;
+      color:#fff;
+      display:inline-block;
+      text-align:center;
+      width:46px;
+      height:46px;
+      vertical-align: middle;
+      line-height:46px;
+      font-size:34px;
+      margin: -10px 0;
+    }
+    h2{
+      font-size: 20px;
+      margin-left: 10px;
+      color: #666;
+      font-weight: normal;
+    }
+    img,h2{
+      display:inline-block;
+      vertical-align: middle;
+    }
+    img{
+      width:100px;
+    }
+    .wrapper{
+      padding:20px 0;
+    }
+    &:after{
+      display:block;
+      content:'';
+      height:4px;
+      background: #15a9da; /* Old browsers */
+      background: -moz-linear-gradient(left, #15a9da 1%, #48c347 27%, #0fa6ea 100%); /* FF3.6-15 */
+      background: -webkit-linear-gradient(left, #15a9da 1%,#48c347 27%,#0fa6ea 100%); /* Chrome10-25,Safari5.1-6 */
+      background: linear-gradient(to right, #15a9da 1%,#48c347 27%,#0fa6ea 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+      filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#15a9da', endColorstr='#0fa6ea',GradientType=1 ); /* IE6-9 */
+    }
+  }
+</style>
 <style lang="scss">
   .frame-docs{
     .sidebar-scrollbar-wrap{
